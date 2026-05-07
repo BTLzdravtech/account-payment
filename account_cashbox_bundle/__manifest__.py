e985fea079005efd14cc8651cd0d9c1ae0e36e69
fcarlini <felipercarlini@gmail.com>
2026-05-07 09:43:15 -0300

    update manifest

--- account_cashbox_bundle/__manifest__.py
+++ account_cashbox_bundle/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Cashbox Bundle",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.1.1",
     "category": "Accounting",
     "summary": "Technical bridge module for account_cashbox and l10n_ar_payment_bundle",
     "author": "ADHOC SA",
