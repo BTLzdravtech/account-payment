8b72bb0854bd706b2a333aeef22730c3cdc3aba8
alain <alainbloos@gmail.com>
2026-06-10 19:01:28 -0300

    [FIX] account_payment_financial_surcharge: preserve payment button reference

--- account_payment_financial_surcharge/__manifest__.py
+++ account_payment_financial_surcharge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Payments with Financial Surchange",
-    "version": "18.0.1.1.1",
+    "version": "18.0.1.1.2",
     "author": "ADHOC SA",
     "license": "AGPL-3",
     "category": "Payment",
